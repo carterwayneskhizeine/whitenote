29e9d0c14da32b4fa3c962e5b4449584d1866d73
KK <got.money@qq.com>
2026-01-04 22:08:16 +0800

    feat: add MainLayout component with left and right sidebars and mobile navigation

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
       <div className="flex w-full max-w-[1300px] relative">
         <LeftSidebar />
 
-        <main className="flex-1 border-r border-border min-h-screen flex flex-col max-w-[600px]">
+        <main className="flex-1 border-x border-border min-h-screen flex flex-col max-w-[600px]">
           <MobileNav />
           {children}
         </main>
